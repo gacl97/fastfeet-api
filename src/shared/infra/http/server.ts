41146410a824588d45b4a7753b3651796997f52a
Gustavo <gacl@ic.ufal.br>
2020-06-27 21:28:33 -0300

    Add cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'; // Utilizar sintaxe de decorators
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -13,6 +14,7 @@ import '@shared/infra/typeorm';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
